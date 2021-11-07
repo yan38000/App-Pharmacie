f1cc1d901fc1ef247883122a74da2a04f36dd54b
yan38000 <61967417+yan38000@users.noreply.github.com>
2021-11-07 20:13:42 +0100

    Delete pharmacie-api directory

--- pharmacie-api/model/db.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const sqlite = require('sqlite3');
-const COLOR = require('../lib/strcolors');
-
-
-module.exports.dbInit = (callback) => {
-    let db = new sqlite.Database('./model/localdb.db', callback);
-    return db;
-}
