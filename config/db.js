6b7f10a4c29e1c08198a07ad71b7a287c8b06900
yan38000 <61967417+yan38000@users.noreply.github.com>
2021-11-09 11:22:00 +0100

    Update code(ERR)
    Modification du code validation de la console.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const color  = require('./strcolors')
 
 mongoose.connect("mongodb+srv://"+process.env.DB_IDT_PASS+"@cluster0.sqive.mongodb.net/app-pharamacie",
 {
     useNewUrlParser : true
 }
 )
-.then(()=> console.log('connected to mongoDB'))
-.catch((err)=> console.log('Failed to connected', err));
+.then(()=> console.log(color.STATUS_OK + 'connected to mongoDB'))
+.catch((err)=> console.log(color.STATUS_ERR + 'Failed to connected', err));
