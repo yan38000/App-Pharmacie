f863cfca1c07a2d49f66dbfca7439435aa055286
gitWilhelm <wilhelm.schmit.pro@gmail.com>
2021-11-09 11:05:51 +0100

    Commentaires francisés

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Dependancies
+// Dépendances
 const express = require('express');
 const cors = require('cors');
 
@@ -11,19 +11,20 @@ require('dotenv').config({path : './config/.env'});
 
 require('./config/db');
 
-// Middlewares inits
+// Initialisations middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors());
 
-// Router handling
+// Gestion routeur "patient"
 app.use('/api/patient', patientRouter);
 
-// 400 No route handling
+// 400 Erreur : pas de route
 app.use((req,res)=>{
     res.status(400).send('error no route');
 })
-// Server listen
+
+// Serveur écoute
 app.listen(process.env.PORT, ()=>{
     console.log(color.STATSUS_OK + `Server listening at port ${process.env.PORT}`);
 });
