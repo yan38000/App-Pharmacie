5938008f092af9743361a739bbdf74153dd99468
yan38000 <61967417+yan38000@users.noreply.github.com>
2021-11-17 22:29:05 +0100

    REACT

--- client/src/views/Patient/Patient.js
+++ client/src/views/Patient/Patient.js
@@ -2,10 +2,14 @@ import React, {useState, useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import Axios from 'axios'
 import ButtonPatientsItem from '../../components/patientItem/ButtonPatientsItem'
+import './Patient.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import  {faPlus,faTrash,faEdit} from '@fortawesome/free-solid-svg-icons'
+import {ImageList}from '@mui/material'
 
 export default function Patient() {
    const [patient , setPatient] = useState([]);
- 
+    
 
    useEffect(()=>{
        Axios.get('http://localhost:5000/api/patient/allPatient').then(res =>{
@@ -20,24 +24,43 @@ export default function Patient() {
        
    }, [])
     
-    
    
-
+    
         
 
-   const navigate = useNavigate()
+   
     return (
         
         <div>
             <ButtonPatientsItem />
+            <ImageList sx={{ width: 1100, height: 450 }} cols={2} rowHeight={164} className="scroll">
             {patient.map(patients=>(
-                <div>
-                    <h4 key="_id">{patients.nom}</h4>
+                <div className="all">
+                    <div className="card-all">
+                        <div className="card-s">
+                            <div className="card-left">
+                                <p className="card-title">{patients.nom}</p>
+                                <p className="card-subTitle">Date de naissance :</p>
+                                <p className="card-subTitle">Mutuelle :</p> 
+                                <p className="card-subTitle">Sécurité Social :</p>
+                                <p className="card-subTitle">Ordonnances :</p>
+                            </div>
+                    
+                            <div className="card-right">
+                                <FontAwesomeIcon icon={faPlus}  className="icon-button-card"/>
+                                <FontAwesomeIcon icon={faEdit}  className="icon-button-card"/> 
+                                
+                                <FontAwesomeIcon icon={faTrash}  className="icon-button-card"/>
+                                 
+                            </div>
+
+                         </div>
+                    </div>
                    
                 </div>
                 
             ))}
-            
+            </ImageList>
         </div>
     )
 }
