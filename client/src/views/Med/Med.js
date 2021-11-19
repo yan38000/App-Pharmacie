a7f1b2ee26267d9d8cbf42e71325a374f22e95e5
yan38000 <61967417+yan38000@users.noreply.github.com>
2021-11-19 14:03:58 +0100

    react
    med

--- client/src/views/Med/Med.js
+++ client/src/views/Med/Med.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios'
+import './Med.css'
 
-export default function Med() {
+import {ImageList}from '@mui/material'
+
+export default function Patient() {
+   const [medecin , setMedecin] = useState([]);
+    
+
+   useEffect(()=>{
+       Axios.get('http://localhost:5000/api/medecin/allMedecin').then(res =>{
+           console.log(res)
+           setMedecin(res.data)
+       })
+       .catch(err=>{
+           console.log(err)
+       })
+
+       
+       
+   }, )
+    
+   
+    
+        
+
+   
     return (
+        
         <div>
-            medecin
+            
+            <ImageList sx={{ width: 1100, height: 450 }} cols={2} rowHeight={164} className="scroll">
+            {medecin.map(medecins=>(
+                <div className="all">
+                    <div className="card-all">
+                        <div className="card-s" key={medecins._id}>
+                            <div className="card-left">
+                                <p className="card-title">{medecins.nom} {medecins.prenom}</p>
+                                
+                            </div>
+                    
+                            
+                         </div>
+                    </div>
+                   
+                </div>
+                
+            ))}
+            </ImageList>
         </div>
     )
 }
--- client/src/views/Medic/Medic.js
+++ client/src/views/Medic/Medic.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Medic() {
     return (
         <div>
-            medicament
+            medic
         </div>
     )
 }
